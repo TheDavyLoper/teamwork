682b9eb10c6b4dc3ecac6a2e5eb0e70539d836be
David Akpan <daviduwana@gmail.com>
2019-11-12 01:49:00 +0100

    ft-createUser-route user can sign up

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,20 @@
-const express = require('express')
+const express = require('express');
+const authRoute = require('./routes/authRoute');
+const client = require('./db/queries')
 
 const app = express();
+
 app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+// Routes
+app.use('/api/v1/auth/create-user', authRoute)
+// app.use('/api/v1/articles', articleRoute);
+// app.use('/api/v1/gifs', gifRoute);
+// app.use('/api/v1/feed', feedRoute);
 
 
-
-const port = 3000;
+const port = process.env.PORT;
 app.listen(port, () => console.log(`App running on port ${port}`));
+
+
--- backend/spec/app.spec.js
+++ backend/spec/app.spec.js
@@ -1,1 +1,7 @@
-	
+const port = require('../app');
+
+describe('testing the listen port', () => {
+  it('port should be 3000', () => {
+    expect(port).toBe(3000);
+  })
+})
