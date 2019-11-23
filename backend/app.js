441c99b3d08ba18fee34716c1d5fc78d7edf2eeb
David A. Akpan <daviduwana@gmail.com>
2019-11-24 00:55:07 +0100

    Revert "Feature bcrypt compare"

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,15 @@
 const express = require('express');
-const cors = require('cors');
 require('dotenv').config();
-const authRoute = require('./routes/authRoute')
-const articleRoute = require('./routes/articleRoute')
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
-app.use(cors());
+app.use(express.urlencoded({extended: false}));
 
 
-app.use('/api/v1/auth/', authRoute);
-app.use('/api/v1/article', articleRoute);
 
 
 const port = process.env.PORT;
 app.listen(port, () => console.log(`App running on port ${port}`));
 
-module.exports = app
+
--- backend/db/config.js
+++ backend/db/config.js
@@ -1,19 +1,13 @@
-const pg = require('pg');
+require('dotenv').config()
 
+const { Pool } = require('pg')
+const isProduction = process.env.NODE_ENV === 'production'
 
-const conString = process.env.conString; //Can be found in the Details page
-const client = new pg.Client(conString);
-client.connect(function(err) {
-  if(err) {
-    return console.error('could not connect to postgres', err);
-  }
-  client.query('SELECT NOW() AS "theTime"', function(err, result) {
-    if(err) {
-      return console.error('error running query', err);
-    }
-    console.log('Connected successfully');
-   
-  });
-});
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 
-module.exports = client
+const pool = new Pool({
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  ssl: isProduction,
+})
+
+module.exports = { pool }
